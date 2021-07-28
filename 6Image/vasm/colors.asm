6758f72751c7743c1d5e67914ba443a20bd456c8
Ilia Prokhorov (Demensdeum) <demensdeum@gmail.com>
2021-07-28 09:00:44 +0300

    includes

---
+++ 6Image/vasm/colors.asm
@@ -0,0 +1,18 @@
+Colors:
+  dc.w $0CCA
+  dc.w $0CCA
+  dc.w $0CAA
+  dc.w $0A86
+  dc.w $0C42
+  dc.w $0EEE
+  dc.w $0444
+  dc.w $022C
+	dc.w $0868
+  dc.w $0A88
+  dc.w $0CAC
+  dc.w $04AC
+  dc.w $08AC
+  dc.w $0484
+  dc.w $0AEE
+  dc.w $0ACA
+ 
